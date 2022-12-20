0e3dd0f37b63e3ee8369cfb6acd7bc09dca23278
shopno <shopnoshutrodhar@gmail.com>
2022-12-21 00:40:29 +0600

    username and password div added

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer')
-const BASE_URL="https://instagram.com/"
+const BASE_URL = "https://instagram.com/"
 const instagram = {
     browser: null,
     page: null,
@@ -9,15 +9,22 @@ const instagram = {
             executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
 
         });
-        instagram.page=await instagram.browser.newPage();
+        instagram.page = await instagram.browser.newPage();
     },
-    login: async (username,password)=>{
-        await instagram.page.goto(BASE_URL,{waitUntil:'networkidle2'});
-        let loginButton =await instagram.page.$x("//div[contains(text(), 'Log in')]")
+    login: async (username, password) => {
+        await instagram.page.goto(BASE_URL, { waitUntil: 'networkidle2' });
+        // await instagram.page.waitFor(1000);
+
+        await instagram.page.waitForNavigation({ waitUntil: 'networkidle2' })
+        await instagram.page.type('input[name="username"]', username, { delay: 50 })
+        await instagram.page.type('input[name="password"]', password, { delay: 50 })
+        /*click*/
+        let loginButton = await instagram.page.$x("//div[contains(text(), 'Log in')]")
         console.log(loginButton)
-        loginButton[0].click()
+        loginButton[0].click();
+        /*clcik*/
         debugger;
     }
 }
 
-module.exports=instagram;
+module.exports = instagram;
